adbe7d1a963c9a03e430b6dd4eed8e22af6b2f2d
xxrose <oleg419583@yandex.ru>
2021-09-12 15:09:19 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import FooterMenu from "./src/footer-menu/footer-menu";
+import EvCalc from "./src/ev-calc/EvCalc";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+  render() {
+    return (
+        <View style={styles.container}>
+          <View style={styles.header}></View>
+            <EvCalc />
+            <FooterMenu  />
+        </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'flex-start'
   },
+  header : {
+      height : 50,
+      width : '100%',
+      backgroundColor: '#36454F',
+      zIndex: 10
+  },
+  footer__menu : {
+      position : 'absolute',
+      bottom : 0,
+      height: 75,
+      width: '100%',
+      backgroundColor: '#36454F',
+
+  }
 });
---
+++ src/footer-menu/footer-menu.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default class FooterMenu extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <View style={styles.footer__menu}>
+                <Text>Bottom menu!</Text>
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    footer__menu : {
+        position : 'absolute',
+        bottom : 0,
+        height: 75,
+        width: '100%',
+        backgroundColor: '#36454F',
+
+    }
+})
